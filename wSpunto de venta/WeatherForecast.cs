b748bff9f9e52091ba1be4afed23eca62a18221e
Nicolas <nicolasmortizg@gmail.com>
2022-06-04 21:00:37 -0500

    Agregar archivos de proyecto.

---
+++ wSpunto de venta/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace wSpunto_de_venta
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
